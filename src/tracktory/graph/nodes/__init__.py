028b4f752bce21631c594d655231998f06d8bcee
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2026-05-21 13:33:02 +0900

    ✨ [Feat] LLM 설명 생성 노드 (#91)

--- src/tracktory/graph/nodes/__init__.py
+++ src/tracktory/graph/nodes/__init__.py
@@ -5,6 +5,7 @@
 """
 
 from tracktory.graph.nodes.input_normalize import NormalizedProfile, normalize_input
+from tracktory.graph.nodes.llm_explanation import LLMClient, LLMExplanationNode
 from tracktory.graph.nodes.profile_embed import EmbeddingClient, ProfileEmbedNode
 from tracktory.graph.nodes.roadmap import CourseRepository, RoadmapNode
 from tracktory.graph.nodes.track_synergy import TrackRepository, TrackSynergyNode
@@ -12,6 +13,8 @@ from tracktory.graph.nodes.track_synergy import TrackRepository, TrackSynergyNod
 __all__ = [
     "CourseRepository",
     "EmbeddingClient",
+    "LLMClient",
+    "LLMExplanationNode",
     "NormalizedProfile",
     "ProfileEmbedNode",
     "RoadmapNode",
